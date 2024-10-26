057cf588b5efb5b66e0c7ed7ec52a9626e4a6e17
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-26 22:30:06 +0900

    로그인 프론트엔드

--- src/features/user/userSlice.js
+++ src/features/user/userSlice.js
@@ -6,7 +6,18 @@ import { initialCart } from "../cart/cartSlice";
 
 export const loginWithEmail = createAsyncThunk(
   "user/loginWithEmail",
-  async ({ email, password }, { rejectWithValue }) => {}
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await api.post("/auth/login", { email, password });
+      // 성공
+      // LoginPage
+      return response.data;
+    } catch (error) {
+      // 실패
+      // 실패 시 생긴 에러 값을 reducer에 저장
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const loginWithGoogle = createAsyncThunk(
@@ -81,6 +92,18 @@ const userSlice = createSlice({
       })
       .addCase(registerUser.rejected, (state, action) => {
         state.registrationError = action.payload;
+      })
+      .addCase(loginWithEmail.pending, (state) => {
+        state.loading = true;
+      })
+      .addCase(loginWithEmail.fulfilled, (state, action) => {
+        state.loading = false;
+        state.user = action.payload.user;
+        state.loginError = null;
+      })
+      .addCase(loginWithEmail.rejected, (state, action) => {
+        state.loading = false;
+        state.loginError = action.payload;
       });
   },
 });
