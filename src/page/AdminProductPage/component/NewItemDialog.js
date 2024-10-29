e224861ef429cdbac7c0e8e99b66fc357bb6be0b
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-29 16:34:41 +0900

    상품 생성하기

--- src/page/AdminProductPage/component/NewItemDialog.js
+++ src/page/AdminProductPage/component/NewItemDialog.js
@@ -76,6 +76,7 @@ const NewItemDialog = ({ mode, showDialog, setShowDialog }) => {
     }, {});
     if (mode === "new") {
       // 새 상품 만들기
+      dispatch(createProduct({ ...formData, stock: totalStock }));
     } else {
       // 상품 수정하기
     }
