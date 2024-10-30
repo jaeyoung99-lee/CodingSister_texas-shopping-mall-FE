d37de4cf80388a3736d8fc5a81606e7aff707362
jy01.lee <jy01.lee@smarterbuild.net>
2024-10-31 03:36:38 +0900

    LandingPage 원상복구

--- src/page/LandingPage/LandingPage.js
+++ src/page/LandingPage/LandingPage.js
@@ -1,39 +1,23 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect } from "react";
 import ProductCard from "./components/ProductCard";
 import { Row, Col, Container } from "react-bootstrap";
-import { useNavigate, useSearchParams } from "react-router-dom";
+import { useSearchParams } from "react-router-dom";
 import { useDispatch, useSelector } from "react-redux";
 import { getProductList } from "../../features/product/productSlice";
-import ReactPaginate from "react-paginate";
 
 const LandingPage = () => {
   const dispatch = useDispatch();
-  const navigate = useNavigate();
 
-  const { productList, totalPageNum } = useSelector((state) => state.product);
+  const productList = useSelector((state) => state.product.productList);
   const [query] = useSearchParams();
-  const [searchQuery, setSearchQuery] = useState({
-    page: query.get("page") || 1,
-    name: query.get("name") || "",
-    limit: 8, // 한 페이지에 8개씩 보이도록 설정
-  });
-
+  const name = query.get("name");
   useEffect(() => {
-    dispatch(getProductList({ ...searchQuery }));
+    dispatch(
+      getProductList({
+        name,
+      })
+    );
   }, [query]);
-
-  useEffect(() => {
-    if (searchQuery.name === "") {
-      delete searchQuery.name;
-    }
-    const params = new URLSearchParams(searchQuery);
-    const queryString = params.toString();
-    navigate("?" + queryString);
-  }, [searchQuery]);
-
-  const handlePageClick = ({ selected }) => {
-    setSearchQuery({ ...searchQuery, page: selected + 1 });
-  };
 
   return (
     <Container>
@@ -46,36 +30,14 @@ const LandingPage = () => {
           ))
         ) : (
           <div className="text-align-center empty-bag">
-            {searchQuery.name === "" ? (
+            {name === "" ? (
               <h2>등록된 상품이 없습니다!</h2>
             ) : (
-              <h2>{searchQuery.name}과 일치한 상품이 없습니다!`</h2>
+              <h2>{name}과 일치한 상품이 없습니다!`</h2>
             )}
           </div>
         )}
       </Row>
-
-      <ReactPaginate
-        nextLabel="next >"
-        onPageChange={handlePageClick}
-        pageRangeDisplayed={5}
-        pageCount={totalPageNum}
-        forcePage={searchQuery.page - 1}
-        previousLabel="< previous"
-        renderOnZeroPageCount={null}
-        pageClassName="page-item"
-        pageLinkClassName="page-link"
-        previousClassName="page-item"
-        previousLinkClassName="page-link"
-        nextClassName="page-item"
-        nextLinkClassName="page-link"
-        breakLabel="..."
-        breakClassName="page-item"
-        breakLinkClassName="page-link"
-        containerClassName="pagination"
-        activeClassName="active"
-        className="display-center list-style-none"
-      />
     </Container>
   );
 };
