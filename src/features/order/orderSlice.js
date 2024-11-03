4d6392eacf794b74691c8904798a21cf78bf9055
jy01.lee <jy01.lee@smarterbuild.net>
2024-11-04 03:16:06 +0900

    오더 생성하기 프론트엔드 2탄

--- src/features/order/orderSlice.js
+++ src/features/order/orderSlice.js
@@ -16,7 +16,18 @@ const initialState = {
 // Async thunks
 export const createOrder = createAsyncThunk(
   "order/createOrder",
-  async (payload, { dispatch, rejectWithValue }) => {}
+  async (payload, { dispatch, rejectWithValue }) => {
+    try {
+      const response = await api.post("/order", payload);
+      if (response.status !== 200) {
+        throw new Error(response.error);
+      }
+      return response.data.orderNum;
+    } catch (error) {
+      dispatch(showToastMessage({ status: "error", message: error.error }));
+      return rejectWithValue(error.error);
+    }
+  }
 );
 
 export const getOrder = createAsyncThunk(
@@ -43,7 +54,21 @@ const orderSlice = createSlice({
       state.selectedOrder = action.payload;
     },
   },
-  extraReducers: (builder) => {},
+  extraReducers: (builder) => {
+    builder
+      .addCase(createOrder.pending, (state, action) => {
+        state.loading = true;
+      })
+      .addCase(createOrder.fulfilled, (state, action) => {
+        state.loading = false;
+        state.error = "";
+        state.orderNum = action.payload;
+      })
+      .addCase(createOrder.rejected, (state, action) => {
+        state.loading = false;
+        state.error = action.payload;
+      });
+  },
 });
 
 export const { setSelectedOrder } = orderSlice.actions;
